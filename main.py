ecb425334b640a41c645effcc39b9da99557438f
christiaan-janssen <christiaanjanssen@outlook.com>
2024-09-07 08:12:54 +0200

    feat: Add some input

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ pygame.init()
 screen = pygame.display.set_mode((1200,720))
 clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # Poll for events
@@ -15,11 +18,22 @@ while running:
     # fill the screen
     screen.fill("purple")
 
-    # RENDER THE GAME
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
 
     # flip() the display to put stuff on the screen
     pygame.display.flip()
 
-    clock.tick(60) # limit FPS to 60
+    # limit FPS to 60
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
